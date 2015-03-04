1acb7923f3b7d121b99f4a4da77486e5bbe97604
Brian Schlenker <bschlenk@umich.edu>
2015-03-03 22:27:22 -0800

    added server code; updated .gitignore with node ignores

---
+++ server/index.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+var express = require('express')
+  , bodyParser = require('body-parser')
+  , multer = require('multer')
+  , path = require('path')
+  , app = express()
+  ;
+
+
+var PORT = process.env.PORT || 3000;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ dest: './uploads' }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/upload', function(req, res) {
+    console.log('file name:', req.files.file.name);
+    console.log('file path:', req.files.file.path);
+    res.sendStatus(204);
+});
+
+app.get('/', function(req, res) {
+    res.render('upload_form.jade', { title: 'Upload File' });
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+app.listen(PORT, function() {
+    console.log('Listening on port', PORT);
+});
